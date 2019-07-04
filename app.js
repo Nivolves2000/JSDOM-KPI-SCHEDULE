4e9024351953bb5157896be04c2ba970f7b075e7
Danya Dubich <d.dubovyk@nodeart.io>
2019-07-05 00:07:20 +0300

    add: web hooks

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const jsdom = require( 'jsdom' );
 const curl = require( 'curl' );
 const FormData = require( 'form-data' );
-const request = require( 'request' );
+const Koa = require( 'koa' );
+const Router = require( 'koa-router' );
+const bodyParser = require( 'koa-bodyparser' );
+const TelegramBot = require( 'node-telegram-bot-api' );
 
-
+const port = 4000;
+const BOT_TOKEN = '838448255:AAG-BTbYzV0hA1IK-tt7ym-3k_hWxE4ifOI';
+const webHookUrl = 'https://b52dbd0d.ngrok.io';
 const rozklad = 'http://rozklad.kpi.ua';
 const schedules = '/Schedules/';
+const url = 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx';
 const days = [ 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота' ];
 let schedule = {
   '1': {
@@ -26,11 +32,12 @@ let schedule = {
   }
 };
 
+const bot = new TelegramBot( BOT_TOKEN, { polling: true } );
+const router = Router();
 const { JSDOM } = jsdom;
+const app = new Koa();
 
-const url = 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx?&mobile=true';
-
-function parceDays( data ) {
+function parceDays( data, chatId ) {
   let dayIndex = 0;
   Object.keys( data ).map( day => {
     if ( ( !days.includes( data[ day ].textContent ) ) && ( day !== '0' ) && ( day !== '49' ) ) {
@@ -45,21 +52,23 @@ function parceDays( data ) {
       }
     }
   }, dayIndex, schedule );
-  console.log( schedule );
+
+  console.log( `${ Object.keys( schedule ).map( week => `${ week }й тиждень\n
+  ${Object.keys( schedule[ week ].map( day => `${ day }\n
+  ${schedule[ week ][ day ].map( lesson => `${ lesson }` ) }` ) ) }` ) }` );
 }
 
-function parseData( html ) {
+function parseData( html, chatId ) {
   const { JSDOM } = jsdom;
   const dom = new JSDOM( html );
   const { window: { document } } = dom;
-  parceDays( document.querySelectorAll( 'table tr td' ) );
+  parceDays( document.querySelectorAll( 'table tr td' ), chatId );
 }
 
-
-function getUrl( url ) {
-  curl.get( `${ rozklad }${ url }`, null, ( err, resp, body ) => {
+function getUrl( url, chatId ) {
+  curl.get( `${ rozklad } ${ url } `, null, ( err, resp, body ) => {
     if ( resp.statusCode == 200 ) {
-      parseData( body );
+      parseData( body, chatId );
     }
     else {
       console.error( 'error while fetching url' );
@@ -68,7 +77,8 @@ function getUrl( url ) {
 }
 
 
-function parseForm( html ) {
+
+function parseForm( html, group, chatId ) {
   const dom = new JSDOM( html );
   const { window: { document } } = dom;
   const input = document.getElementById( 'ctl00_MainContent_ctl00_txtboxGroup' );
@@ -78,26 +88,48 @@ function parseForm( html ) {
 
 
   const form = new FormData();
+  console.log( `${ rozklad }${ schedules }${ submitLink }` );
 
-
-  inputs.forEach( input => form.append( `${ input.getAttribute( 'name' ) }`, `${ input.getAttribute( 'value' ) }` ), [ form ] );
+  inputs.forEach( input => form.append( `${ input.getAttribute( 'name' ) } `, `${ input.getAttribute( 'value' ) } ` ), [ form ] );
 
   form.submit( `${ rozklad }${ schedules }${ submitLink }`, function ( err, res ) {
-    getUrl( res.headers.location );
+    console.log( res.headers );
+    if ( !res.headers.location ) {
+      bot.sendMessage( chatId, 'No group' );
+    } else {
+      getUrl( res.headers.location, chatId );
+    }
   } );
 }
 
-curl.get( url, null, ( err, resp, body ) => {
-  if ( resp.statusCode == 200 ) {
-    parseForm( body );
-  }
-  else {
-    console.error( 'error while fetching url' );
-  }
+function getBody( group, chatId ) {
+  curl.get( url, null, ( err, resp, body ) => {
+    if ( resp.statusCode == 200 ) {
+      parseForm( body, group, chatId );
+    }
+    else {
+      console.error( 'error while fetching url' );
+    }
+  } );
+}
+
+bot.setWebHook( `${ webHookUrl } /bot` );
+
+router.post( '/bot', ctx => {
+  ctx.status = 200;
 } );
 
+app.use( bodyParser() );
+app.use( router.routes() );
 
+app.listen( port, () => {
+  console.log( 'server started' );
+} );
 
-
+bot.on( 'message', ( msg ) => {
+  const chatId = msg.chat.id;
+  const group = msg.text;
+  getBody( group, chatId, url );
+} );
 
 
