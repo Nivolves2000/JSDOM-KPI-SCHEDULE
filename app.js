94757f7aa7791667be07a971ab1b1e094a255ff4
Danya Dubich <d.dubovyk@nodeart.io>
2019-07-05 16:00:31 +0300

    add: telegram bot

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const jsdom = require( 'jsdom' );
 const curl = require( 'curl' );
 const FormData = require( 'form-data' );
-const Koa = require( 'koa' );
+const Koa = require( 'Koa' );
 const Router = require( 'koa-router' );
-const bodyParser = require( 'koa-bodyparser' );
+var bodyParser = require( 'body-parser' );
 const TelegramBot = require( 'node-telegram-bot-api' );
 
-const port = 4000;
-const BOT_TOKEN = '838448255:AAG-BTbYzV0hA1IK-tt7ym-3k_hWxE4ifOI';
-const webHookUrl = 'https://b52dbd0d.ngrok.io';
+
+const TOKEN = '';
+const port = 5000;
 const rozklad = 'http://rozklad.kpi.ua';
+const ngrokUrl = '';
 const schedules = '/Schedules/';
-const url = 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx';
+const url = 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx?&mobile=true';
+
 const days = [ 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота' ];
 let schedule = {
   '1': {
@@ -32,10 +34,12 @@ let schedule = {
   }
 };
 
-const bot = new TelegramBot( BOT_TOKEN, { polling: true } );
-const router = Router();
 const { JSDOM } = jsdom;
 const app = new Koa();
+const router = Router();
+
+
+const bot = new TelegramBot( TOKEN, { polling: true } );
 
 function parceDays( data, chatId ) {
   let dayIndex = 0;
@@ -52,10 +56,9 @@ function parceDays( data, chatId ) {
       }
     }
   }, dayIndex, schedule );
-
-  console.log( `${ Object.keys( schedule ).map( week => `${ week }й тиждень\n
-  ${Object.keys( schedule[ week ].map( day => `${ day }\n
-  ${schedule[ week ][ day ].map( lesson => `${ lesson }` ) }` ) ) }` ) }` );
+  bot.sendMessage( chatId, `${ Object.keys( schedule ).map( week => `\n${ week }й тиждень 
+  ${Object.keys( schedule[ week ] ).map( day => `\n${ day }
+  ${schedule[ week ][ day ].map( ( lesson, index ) => ( `\n${ index + 1 }) ${ lesson }` ) ) }` ) }` ) }`, { parse_mode: 'Markdown' } );
 }
 
 function parseData( html, chatId ) {
@@ -65,8 +68,9 @@ function parseData( html, chatId ) {
   parceDays( document.querySelectorAll( 'table tr td' ), chatId );
 }
 
+
 function getUrl( url, chatId ) {
-  curl.get( `${ rozklad } ${ url } `, null, ( err, resp, body ) => {
+  curl.get( `${ rozklad }${ url }`, null, ( err, resp, body ) => {
     if ( resp.statusCode == 200 ) {
       parseData( body, chatId );
     }
@@ -77,35 +81,33 @@ function getUrl( url, chatId ) {
 }
 
 
-
-function parseForm( html, group, chatId ) {
+function parseForm( html, chatId, group ) {
   const dom = new JSDOM( html );
   const { window: { document } } = dom;
   const input = document.getElementById( 'ctl00_MainContent_ctl00_txtboxGroup' );
   const submitLink = document.getElementById( 'aspnetForm' ).getAttribute( 'action' );
-  input.setAttribute( 'value', 'БС-73' );
+  input.setAttribute( 'value', group );
   const inputs = document.querySelectorAll( '#aspnetForm input' );
 
 
   const form = new FormData();
-  console.log( `${ rozklad }${ schedules }${ submitLink }` );
 
-  inputs.forEach( input => form.append( `${ input.getAttribute( 'name' ) } `, `${ input.getAttribute( 'value' ) } ` ), [ form ] );
+
+  inputs.forEach( input => form.append( `${ input.getAttribute( 'name' ) }`, `${ input.getAttribute( 'value' ) }` ), [ form ] );
 
   form.submit( `${ rozklad }${ schedules }${ submitLink }`, function ( err, res ) {
-    console.log( res.headers );
-    if ( !res.headers.location ) {
-      bot.sendMessage( chatId, 'No group' );
-    } else {
+    if ( res.headers.location ) {
       getUrl( res.headers.location, chatId );
+    } else {
+      bot.sendMessage( chatId, 'No group' );
     }
   } );
 }
 
-function getBody( group, chatId ) {
+function parceSchegle( chatId, group ) {
   curl.get( url, null, ( err, resp, body ) => {
     if ( resp.statusCode == 200 ) {
-      parseForm( body, group, chatId );
+      parseForm( body, chatId, group );
     }
     else {
       console.error( 'error while fetching url' );
@@ -113,23 +115,24 @@ function getBody( group, chatId ) {
   } );
 }
 
-bot.setWebHook( `${ webHookUrl } /bot` );
 
-router.post( '/bot', ctx => {
-  ctx.status = 200;
-} );
 
-app.use( bodyParser() );
-app.use( router.routes() );
-
-app.listen( port, () => {
-  console.log( 'server started' );
-} );
+bot.setWebHook( `${ ngrokUrl }/bot` );
 
 bot.on( 'message', ( msg ) => {
   const chatId = msg.chat.id;
   const group = msg.text;
-  getBody( group, chatId, url );
+  parceSchegle( chatId, group );
 } );
 
+router.post( '/bot', ctx => {
+  const { body } = ctx.request;
+  console.log( body );
+  bot.processUpdate( body );
+  ctx.status = 200;
+} );
 
+app.use( router.routes() );
+app.use( bodyParser );
+
+app.listen( port, () => console.log( 'server started' ) );
